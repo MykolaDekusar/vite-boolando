3f727551bea2d38940d1fe4a75e26550b3a4bfc0
MykolaDekusar <ndekusar97@gmail.com>
2024-05-30 15:38:03 +0200

    Added main Container, card Components and Footer component with implementation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppCard from './components/AppCard.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default {
     name: "Main",
     components: {
     AppHeader,
+    AppCard,
+    AppFooter,
 },
 };
 
 </script>
 
 <template>
+    <!-- header -->
   <AppHeader />
+  <!-- main -->
+  <main>
+    <div class="container">
+        <div class="row-2 flex wrap gap-25">
+            <AppCard/>
+            <AppCard/>
+            <AppCard/>
+            <AppCard/>
+            <AppCard/>
+            <AppCard/>
+        </div>
+    </div>
+  </main>
+  <!-- footer -->
+  <AppFooter />
 </template>
 
 <style>
